8ab00a54f7be925c8998f3f48e2678d154bfaa04
Marius Rundereim <marius.rundereim@gmail.com>
2024-01-11 12:23:30 +0100

    hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,23 @@
   <title>Home</title>
 </svelte:head>
 
+
+<div class="hero-img">
+  <div>
+    <h1>Thomas Hallan</h1>
+    <h2>Graphic Designer</h2>
+    <button>Contact Me</button>
+  </div>
+</div>
+
+<style>
+  .hero-img{
+    background-image: url(../lib/images/hero-image.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    filter: brightness(0.5);
+    width: 100%;
+    min-height: 32rem;
+  }
+</style>
