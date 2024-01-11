e928ddfab2c65f32107c273f62a30b854795df2f
Marius Rundereim <marius.rundereim@gmail.com>
2024-01-11 09:20:22 +0100

    minor things

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,7 +19,7 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+			<li aria-current={$page.url.pathname === '/projects' ? 'page' : undefined}>
 				<a href="/projects">Projects</a>
 			</li>
 		</ul>
