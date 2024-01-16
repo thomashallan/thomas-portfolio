faff83a3f8c863fe44532f05cb4373b84e54ccd3
StineNygren <107799331+StineNygren@users.noreply.github.com>
2024-01-16 10:06:12 +0100

    data made, need to change images later

---
+++ src/lib/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    id: 0,
+    title: "Palmesus Concept",
+    text: "Palmesus is the biggest beach party in Scandinavia. They contacted us at Noroff University and asked if we could design some concepts for them. This is my submission.",
+    image: [
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Ravnedalen Concept",
+    text: "Ravnedalen Live is a music festival located in Kristiansand, Norway. The music festival is the most sustainable festival in Norway. They contacted Noroff for some concepts. This is my submission.",
+    image: [
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Product photography Casio",
+    text: "We has a school assignment where our task was to take professional product. I chose a Casio watch and here is my result.",
+    image: [
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "i-D Magazine Cover",
+    text: "A photography asssignment in my first year at Noroff. The assignment was to take a picture in the style of an i-D magazine cover. Here is my submission.",
+    image: [
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+      {
+        url: "https://picsum.photos/1000/500",
+        alt: "Project 1",
+      },
+    ],
+  },
+];
