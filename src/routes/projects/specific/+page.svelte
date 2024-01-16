163da4339439bc96d7123c8e89e083d33650af8c
Marius Rundereim <marius.rundereim@gmail.com>
2024-01-16 18:10:13 +0100

    starting specific page

---
+++ src/routes/projects/specific/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    import { projects } from '$lib/data.js'
+</script>
+
+<svelte:head>
+	<title>Single projects</title>
+</svelte:head>
+
+
+{@debug projects}
+<h1>Specific page</h1>
+<p>Text specific</p>
