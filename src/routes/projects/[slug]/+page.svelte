90cd17a32ce384b057784effea957ec7b7c63036
Marius Rundereim <marius.rundereim@gmail.com>
2024-01-17 09:50:27 +0100

    try to understand specific code page

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -5,17 +5,22 @@
     import { page } from '$app/stores';
     import { derived } from 'svelte/store';
 
+    // Creates a derived store name automatically updates value to match the slug
     const slug = derived(page, $page => $page.params.slug);
+    
 
+    console.log(slug)
     const project = derived([slug, projects], ([$slug, $projects]) => $projects.find(p => p.id.toString() === $slug));
+    console.log(project)
 </script>
 
 <svelte:head>
-	<title>Single projects</title>
+    <title>{$project.title}</title>
 </svelte:head>
 
 
-<!-- {@debug projects} -->
+{@debug slug}
+{@debug project}
 <h1>Specific Page</h1>
 {#if $project}
     <h2>{$project.title}</h2>
