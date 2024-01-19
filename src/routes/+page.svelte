5b14ac9371e742209ee9ae077d710acd5c4fd1bb
Marius Rundereim <marius.rundereim@gmail.com>
2024-01-19 14:56:43 +0100

    custom header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,11 @@
   <title>Home</title>
 </svelte:head>
 
-
-
-<section class="hero-img w-full">
+<section class="hero-img ">
   <div class="bg-slate-950/60">
-    <div class="text-white  grid-cols-1 grid justify-items-end items-center w-full min-h-[32rem]">
+    <div class="text-white grid items-center w-full min-h-dvh">
       <div class="space-y-2 md:px-20 text-center">
-        <h1 class="text-5xl font-bold uppercase">Thomas Hallan</h1>
+        <h1 class="text-7xl font-bold uppercase">Thomas Hallan</h1>
         <h2 class="text-3xl uppercase">Graphic Designer</h2>
         <button class="text-white border-white">Contact Me</button>
       </div>
